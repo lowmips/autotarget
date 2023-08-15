5f516a3d30a2f8c526825a46888f410c872f06a1
reggie <reggie@lowmips.com>
2023-08-15 18:00:00 -0400

    dev

--- js/datafeed.js
+++ js/datafeed.js
@@ -1,6 +1,19 @@
 export default {
     onReady: (callback) => {
         console.log('[onReady]: Method call');
+        const configurationData = {
+            // Represents the resolutions for bars supported by your datafeed
+            supported_resolutions: ['1H','6H','12H', '1D', '1W', '1M'],
+            // The `exchanges` arguments are used for the `searchSymbols` method if a user selects the exchange
+            exchanges: [
+                { value: 'MEXC', name: 'MEXC', desc: 'MEXC'},
+            ],
+            // The `symbols_types` arguments are used for the `searchSymbols` method if a user selects this symbol type
+            symbols_types: [
+                { name: 'crypto', value: 'crypto'}
+            ]
+        };
+        setTimeout(() => callback(configurationData));
     },
     searchSymbols: (userInput, exchange, symbolType, onResultReadyCallback) => {
         console.log('[searchSymbols]: Method call');
