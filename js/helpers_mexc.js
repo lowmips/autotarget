6377eaa6f563bb860883364b57e7cd87e48b3c46
reggie <reggie@lowmips.com>
2023-08-15 19:19:08 -0400

    dev

--- js/helpers_mexc.js
+++ js/helpers_mexc.js
@@ -1,7 +1,11 @@
 // Makes requests to MEXC API
 export async function makeApiRequest(path) {
     try {
-        const response = await fetch(`https://api.mexc.com/${path}`);
+        const response = await fetch(`https://api.mexc.com/${path}`,{
+            headers:{
+                'X-Proxy-Url': window.location.href + 'php-cross-domain-proxy/proxy.php',
+            },
+        });
         return response.json();
     } catch(error) {
         throw new Error(`MEXC request error: ${error.status}`);
