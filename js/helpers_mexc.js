7dc8894594489c188657dd8af251cc69cd943ec7
reggie <reggie@lowmips.com>
2023-08-16 10:07:26 -0400

    dev

--- js/helpers_mexc.js
+++ js/helpers_mexc.js
@@ -1,4 +1,18 @@
 // Makes requests to MEXC API
+
+export const configurationData =  {
+    // Represents the resolutions for bars supported by your datafeed
+    supported_resolutions: ['1H','6H','12H', '1D', '1W', '1M'],
+        // The `exchanges` arguments are used for the `searchSymbols` method if a user selects the exchange
+        exchanges: [
+        { value: 'MEXC', name: 'MEXC', desc: 'MEXC'},
+    ],
+        // The `symbols_types` arguments are used for the `searchSymbols` method if a user selects this symbol type
+        symbols_types: [
+        { name: 'crypto', value: 'crypto'}
+    ]
+}
+
 export async function makeApiRequest(path) {
     try {
         const response = await fetch(window.location.href + 'php-cross-domain-proxy/proxy.php',{
