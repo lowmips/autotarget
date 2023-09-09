e6546e150733816a067b53c627e1b6b811b6b219
reggie <reggie@lowmips.com>
2023-09-09 15:22:09 -0400

    dev

--- ajax-handlers/get_klines.php
+++ ajax-handlers/get_klines.php
@@ -1,7 +1,7 @@
 <?php
 echo "Hi";
-/*error_reporting(E_ALL);
-require_once('defines.php');
+error_reporting(E_ALL);
+/*require_once('defines.php');
 require_once('mysqli.php');
 
 print_r($_REQUEST);
