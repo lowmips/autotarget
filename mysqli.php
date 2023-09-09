4ef73f6f2d130e3f0778ed5d7af438a2d23b4108
reggie <reggie@lowmips.com>
2023-09-09 11:50:43 -0400

    dev

---
+++ mysqli.php
@@ -0,0 +1,12 @@
+<?php
+$credentials = file('variable/mysql_credentials.txt', FILE_IGNORE_NEW_LINES);
+$host = $credentials[0];
+$username = $credentials[1];
+$password = $credentials[2];
+$database = $credentials[3];
+// Connect to the MySQL database
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli($host, $username, $password, $database);
+
+// Check for connection errors
+if ($mysqli->connect_error) die('Connection failed: ' . $mysqli->connect_errori."\n");
