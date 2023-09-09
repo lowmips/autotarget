6e9083c09ff81527f8a4d89d1ece9ec48078d894
reggie <reggie@lowmips.com>
2023-09-09 15:51:02 -0400

    dev

--- ajax-handlers/get_klines.php
+++ ajax-handlers/get_klines.php
@@ -3,4 +3,7 @@ error_reporting(E_ALL);
 require_once('../defines.php');
 require_once('../mysqli.php');
 
+if(!array_key_exists('resolution', $_REQUEST) || !array_key_exists('from', $_REQUEST) || !array_key_exists('resolutotion', $_REQUEST))
+    die("Missing required request params");
+
 print_r($_REQUEST);
