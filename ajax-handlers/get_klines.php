c98807ef022db07e7b6539b5f10e12038667b268
reggie <reggie@lowmips.com>
2023-09-19 17:03:38 -0400

    dev

--- ajax-handlers/get_klines.php
+++ ajax-handlers/get_klines.php
@@ -27,26 +27,26 @@ $rows = [];
 $loop_ts = $from;
 while($loop_ts < $to){
     $q = "SELECT `open` FROM `btc_usdt_klines_reduced` WHERE `kline_timestamp`=$loop_ts LIMIT 1;";
-    echo $q."<br/>\n";
+    #echo $q."<br/>\n";
     if(($result = $mysqli->query($q)) === false) die("kline query failure");
     if(!is_array($row = $result->fetch_assoc())) die("kline query fetch_assoc failure");
     $open = (float)$row['open'];
 
     $span_end_ts = $loop_ts + ($resolution * 60) - 60;
     $q = "SELECT `close` FROM `btc_usdt_klines_reduced` WHERE `kline_timestamp`=$span_end_ts LIMIT 1;";
-    echo $q."<br/>\n";
+    #echo $q."<br/>\n";
     if(($result = $mysqli->query($q)) === false) die("kline query failure");
     if(!is_array($row = $result->fetch_assoc())) die("kline query fetch_assoc failure");
     $close = (float)$row['close'];
 
     $q = "SELECT MAX(`high`) AS high FROM `btc_usdt_klines_reduced` WHERE `kline_timestamp` BETWEEN $loop_ts AND $span_end_ts";
-    echo $q."<br/>\n";
+    #echo $q."<br/>\n";
     if(($result = $mysqli->query($q)) === false) die("kline query failure");
     if(!is_array($row = $result->fetch_assoc())) die("kline query fetch_assoc failure");
     $high = (float)$row['high'];
 
     $q = "SELECT MIN(`low`) AS low FROM `btc_usdt_klines_reduced` WHERE `kline_timestamp` BETWEEN $loop_ts AND $span_end_ts";
-    echo $q."<br/>\n";
+    #echo $q."<br/>\n";
     if(($result = $mysqli->query($q)) === false) die("kline query failure");
     if(!is_array($row = $result->fetch_assoc())) die("kline query fetch_assoc failure");
     $low = (float)$row['low'];
