dcbba66acebf4636e30a3825a81bae73dfeaa186
reggie <reggie@lowmips.com>
2023-09-19 16:03:15 -0400

    dev

--- ajax-handlers/get_klines.php
+++ ajax-handlers/get_klines.php
@@ -13,6 +13,8 @@ $resolution = $mysqli->real_escape_string($_REQUEST['resolution']);
 $from = $mysqli->real_escape_string($_REQUEST['from']);
 $to = $mysqli->real_escape_string($_REQUEST['to']);
 
+$q = "USE `$database`";
+$result = $mysqli->query($q);
 $q = "CALL get_klines($resolution, $from, $to)";
 $result = $mysqli->query($q);
 #print_r($result);
