bdd6d075166de5594763d248fc379ba81b04b32a
reggie <reggie@lowmips.com>
2023-09-20 05:38:17 -0400

    dev

--- js/datafeed.js
+++ js/datafeed.js
@@ -43,7 +43,7 @@ export default {
         console.log('[getBars]: Method call', symbolInfo, resolution, periodParams );
         const bars = new Array(periodParams.countBack + 1);
         const kline_request_url = window.location.href + 'ajax-handlers/get_klines.php?resolution=' + resolution + '&from=' + periodParams.from + '&to=' + periodParams.to;
-        #console.log('kline_request_url: ' + kline_request_url);
+        //console.log('kline_request_url: ' + kline_request_url);
         //const response = fetch(kline_request_url);
         //console.log(response);
 
