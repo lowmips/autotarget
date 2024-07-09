744c672c91a15a1811f9b7519879510b7bbbc0a8
reggie <reggie@lowmips.com>
2024-07-09 18:16:05 -0400

    dev

--- ajax-handlers/get_klines.php
+++ ajax-handlers/get_klines.php
@@ -46,7 +46,7 @@ while($loop_ts < $to){
     }
     $open = (float)$row['open'];
 
-    // CLOSE -- requested kline migh be an overshoot on a unfinished span -- find the latest close price
+    // CLOSE -- requested kline might be an overshoot on a unfinished span -- find the latest close price
     $q = "SELECT `close` FROM `btc_usdt_klines_reduced` WHERE `kline_timestamp`<=$span_end_ts ORDER BY kline_timestamp DESC LIMIT 1;";
     #echo $q."<br/>\n";
     if(($result = $mysqli->query($q)) === false) die("kline query failure: $q");
