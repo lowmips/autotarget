c24ccc8f9d69726aeff522262205dd1686550991
reggie <reggie@lowmips.com>
2024-07-20 18:10:01 -0400

    dev

--- ajax-handlers/get_klines.php
+++ ajax-handlers/get_klines.php
@@ -59,7 +59,7 @@ while($loop_ts < $to){
     if(!is_array($row = $result->fetch_assoc())) die("kline query fetch_assoc failure: $q");
     $high = (float)$row['high'];
 
-    $q = "SELECT MIN(`low`) AS low FROM `klines_1` WHERE `kline_timestamp` BETWEEN $loop_ts AND $span_end_ts";
+    $q = "SELECT MIN(`low`) AS low FROM `klines_1` WHERE `timestamp` BETWEEN $loop_ts AND $span_end_ts";
     #echo $q."<br/>\n";
     if(($result = $mysqli->query($q)) === false) die("kline query failure: $q");
     if(!is_array($row = $result->fetch_assoc())) die("kline query fetch_assoc failure: $q");
