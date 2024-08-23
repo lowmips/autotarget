4b6c3119878031b73c1e394d2c13a5c9eb598ad7
reggie <reggie@lowmips.com>
2024-08-23 16:58:54 -0400

    dev

--- ws_server/ws_klines.py
+++ ws_server/ws_klines.py
@@ -20,3 +20,22 @@ ssl_cert = config['ssl']['fullchain']
 ssl_key = config['ssl']['privkey']
 ssl_context.load_cert_chain(ssl_cert, keyfile=ssl_key)
 mdb = mysqlDBC(config['mysql']['username'], config['mysql']['password'], config['mysql']['host'], config['mysql']['database'])
+
+async def main_loop():
+    while True:
+        print('main_loop()')
+        await asyncio.sleep(10)
+
+
+async def handle_ws(ws,path):
+    print('websocket:')
+    print(websocket)
+    print('path:')
+    print(path)
+
+async def main():
+    async with asyncio.TaskGroup() as group:
+        group.create_task(main_loop()
+        group.create_task(websockets.serve(handle_ws, "0.0.0.0", 8765, ssl=ssl_context))
+
+asyncio.run(main())
