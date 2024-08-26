46f8d0d2e3d3fc2eb55b44ab106a0b7162372633
reggie <reggie@lowmips.com>
2024-08-26 19:13:31 -0400

    dev

--- js/streaming.js
+++ js/streaming.js
@@ -129,7 +129,7 @@ function getNextBarTime(barTime, resolution){
     console.log('getNextBarTime('+barTime+','+resolution+')');
     const date = new Date(barTime);
     date.setMinutes(date.getMinutes() + resolution);
-    return date.getTime() / 1000;
+    return date.getTime();
 }
 /*function getNextDailyBarTime(barTime) {
     const date = new Date(barTime * 1000);
