ab180d7363cd5e71f65008b5844880e37dd91174
reggie <reggie@lowmips.com>
2024-08-28 12:57:29 -0400

    dev

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 import Datafeed from './datafeed.js';
 
 window.tvWidget = new TradingView.widget({
-    symbol: 'MEXC:BTC/USDT',            // Default symbol pair
-    interval: '60',                        // Default interval
-    fullscreen: true,                      // Displays the chart in the fullscreen mode
     container: 'tv_chart_container',       // Reference to an attribute of a DOM element
     datafeed: Datafeed,
-    library_path: 'charting_library/charting_library/',
     debug: true,
+    fullscreen: true,                      // Displays the chart in the fullscreen mode
+    interval: '60',                        // Default interval
+    library_path: 'charting_library/charting_library/',
+    symbol: 'MEXC:BTC/USDT',            // Default symbol pair
+    theme: "dark",
     timezone: 'America/New_York',
 });
 
