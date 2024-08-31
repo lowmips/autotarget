600c547388d42a7e66abb2ed2aa1eca561a6907d
reggie <reggie@lowmips.com>
2024-08-31 19:24:12 -0400

    dev

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+    <title>AutoTarget - test</title>
+    <script type="text/javascript" src="robust-websocket/robust-websocket.js"></script>
+    <script type="text/javascript">
+        const ws = new RobustWebSocket('wss://www.lowmips.com/autotarget/targets/');
+    </script>
+
+</head>
+
+<body style="margin:0px;">
+
+</body>
+
+</html>
