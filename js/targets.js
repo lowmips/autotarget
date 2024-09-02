d5a76f91de1ed14e41b45a3ad51e06a181dece37
reggie <reggie@lowmips.com>
2024-09-02 10:32:04 -0400

    dev

---
+++ js/targets.js
@@ -0,0 +1,15 @@
+const ws_targets = new RobustWebSocket('wss://www.lowmips.com/autotarget/targets/');
+
+ws_targets.addEventListener('open', function(event) {
+    console.log('ws_targets [open]' + event);
+});
+ws_targets.addEventListener('close', function(event) {
+    console.log('ws_targets [close]: code['+event.code+'] reason['+event.reason+'] wasClean['+event.wasClean+']');
+});
+ws_targets.addEventListener('error', function(event) {
+    console.log('ws_targets [error]: ' + event);
+});
+ws_targets.addEventListener('message', function(event) {
+    console.log('ws_targets [message]: ' + event.data);
+    console.log('    [origin] '+event.origin);
+});
