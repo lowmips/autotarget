bccc68733d45e6d6b30861e079b17565b2ad0651
reggie <reggie@lowmips.com>
2024-09-02 08:31:34 -0400

    dev

--- test.php
+++ test.php
@@ -7,6 +7,20 @@
     <script type="text/javascript" src="robust-websocket/robust-websocket.js"></script>
     <script type="text/javascript">
         const ws = new RobustWebSocket('wss://www.lowmips.com/autotarget/targets/');
+        ws.addEventListener('open', function(event) {
+            console.log('ws [open]');
+        });
+        ws.addEventListener('close', function(event) {
+            console.log('ws [close]: ' + event.data)
+        });
+        ws.addEventListener('error', function(event) {
+            console.log('ws [error]: ' + event.data)
+        });
+        ws.addEventListener('message', function(event) {
+            console.log('ws [message]: ' + event.data)
+        });
+
+
     </script>
 
 </head>
