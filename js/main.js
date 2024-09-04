72341f8c4a9677c375c003be8cb92382231e3c9e
reggie <reggie@lowmips.com>
2024-09-04 12:28:00 -0400

    dev

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ window.tvStuff.widget.subscribe('time_interval', function(a){
     async function bleh(symbol){
         await checkFixDrawingsResolution(symbol);
     }
-    let x = bleh(window.tvStuff.current_symbol);
+    setTimeout(function(){let x = bleh(window.tvStuff.current_symbol);}, 1000);
 });
 
 
