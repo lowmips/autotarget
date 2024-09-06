5d6f59ae81ed1cebca6508b6edb8392c8396c602
reggie <reggie@lowmips.com>
2024-09-06 14:28:27 -0400

    dev

--- js/targets.js
+++ js/targets.js
@@ -232,7 +232,7 @@ async function handleUpdateMsg(msg, sendtoback){
         if(sendtoback && ('sendToBack' in shape)) shape.sendToBack();
         else if('sendToFront' in shape) shape.sendToFront();
 
-        if(target_count < window.tvStuff.targets.filtering.target_count.min) shape.setProperties({visible: false});
+        //if(target_count < window.tvStuff.targets.filtering.target_count.min) shape.setProperties({visible: false});
 
         targetCache[ticker]['shape_id_to_target'][shape_id] = new_target;
         if (!(ts_start in targetCache[ticker]['target_to_shape_id'])) targetCache[ticker]['target_to_shape_id'][ts_start] = {};
