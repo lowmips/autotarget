712fbcfb61af0992c5f411ea9e26870f2e82ce26
reggie <reggie@lowmips.com>
2024-09-21 15:10:32 -0400

    dev

--- js/targets.js
+++ js/targets.js
@@ -127,7 +127,7 @@ async function handleTargetMsg(msg, sendtoback){
             target_count,
         };
 
-        if(target_price != 62690.22) return;
+        //if(target_price != 62690.22) return;
 
         if(target_count < window.tvStuff.targets.requesting.target_count.min) return;
 
