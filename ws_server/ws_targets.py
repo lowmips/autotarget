d2125b03c076a1b7cd7c40455ca2cac40fe67fe9
reggie <reggie@lowmips.com>
2024-09-23 18:53:58 -0400

    dev

--- ws_server/ws_targets.py
+++ ws_server/ws_targets.py
@@ -8,6 +8,13 @@ import sys
 import time
 from zmysql import mysqlDBC
 
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return '`'+str(o)+'`' #` is special, will be removed later
+        return super(DecimalEncoder, self).default(o)
+
 config = None
 mdb = None # mysqlDBC instance
 server_port = None
@@ -114,9 +121,11 @@ async def main_loop():
                  "AND `target_count` > 1 "
                  ).format(m=pair_id)
             latest_rows = mdb.query_get_all(q)
-            if len(latest_rows) > 0:
-                latest_rows_str = json.dumps(latest_rows)
-                latest_ranges_str = json.dumps(pair_id_latest_ranges[pair_id]['ranges'])
+            if len(latest_targets) > 0:
+                #latest_rows_str = json.dumps(latest_rows)
+                latest_rows_str = json.dumps(latest_rows, cls=DecimalEncoder).replace("\"`",'').replace("`\"",'')
+                #latest_ranges_str = json.dumps(pair_id_latest_ranges[pair_id]['ranges'])
+                latest_ranges_str = json.dumps(pair_id_latest_ranges[pair_id]['ranges'], cls=DecimalEncoder).replace("\"`",'').replace("`\"",'')
                 if latest_rows_str != latest_ranges_str:
                     pair_id_latest_ranges[pair_id]['ranges'] = latest_rows
                     has_ranges = True
