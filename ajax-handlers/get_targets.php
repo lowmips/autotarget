80240bc883d9d3a6854ddfc6638c6cd67d48d899
reggie <reggie@lowmips.com>
2025-03-26 16:19:20 -0400

    Dev

--- ajax-handlers/get_targets.php
+++ ajax-handlers/get_targets.php
@@ -124,7 +124,7 @@ $q = "SELECT * ".
     "AND `ts_end`>='$min_found_ts' ".
     "AND `ts_end`<='$max_found_ts' ".
     //"AND `target_count`>='$min_target_count' ".
-    "AND `target_count`>='200' ".
+    "AND `target_count`>='120' ".
     "ORDER BY `ts_end` DESC ";
 #echo $q; exit;
 //$update_obj['dbg'][] = $q;
